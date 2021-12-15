5f093d3863613a3fda69639cfb540b39354a20ca
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-16 00:25:06 +0100

    Object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-    this.value = this.value + 1;
+  this.value = this.value + 1;
 };
 
 myObject.incr();
