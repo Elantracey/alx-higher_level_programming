e16c6682de15719109d1f75e5439892ed82cf82d
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-16 00:16:49 +0100

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-
 function add (a, b) {
-    a = parseInt(a);
-    b = parseInt(b);
-    return a + b;
+  a = parseInt(a);
+  b = parseInt(b);
+  return a + b;
 }
 console.log(add(process.argv[2], process.argv[3]));
