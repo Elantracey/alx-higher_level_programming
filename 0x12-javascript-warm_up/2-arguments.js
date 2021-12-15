88475c7721111f6d3de573ec9434570d99091327
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-12-15 18:57:40 +0100

    warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let n;
+if (process.argv.length <= 2) {
+    n = 'No argument';
+} else if (process.argv.length === 3) {
+    n = 'Argument found';
+} else {
+    n = 'Arguments found';
+}
+console.log(n);
+
