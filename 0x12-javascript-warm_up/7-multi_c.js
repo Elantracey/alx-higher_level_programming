46bc3ccae8e98ae04f045cb4ad4fa94854c217e5
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-16 00:35:05 +0100

    C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
-if (!isNaN(n)) {
-  for (let i = 0; i < n; i++) {
-    console.log('C is fun');
-    }
-} else {
+let i = parseInt(process.argv[2]);
+if (Number.isNaN(i)) {
   console.log('Missing number of occurrences');
+} else {
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
+  }
 }
