b36c590ba5784ff17383e7ace807bfb1b9259cff
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-15 23:29:14 +0100

    Argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 let n;
 if (process.argv.length <= 2) {
-    n = 'No argument';
+  n = 'No argument';
 } else if (process.argv.length === 3) {
-    n = 'Argument found';
+  n = 'Argument found';
 } else {
-    n = 'Arguments found';
+  n = 'Arguments found';
 }
 console.log(n);
 
