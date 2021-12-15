560fc4f0435dd1fee6d0592edf771e28c5d6dd5c
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-15 20:24:33 +0100

    Concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const { readFileSync, writeFileSync } = require('fs');
+const fpA = process.argv[2];
+const fpB = process.argv[3];
+const fpC = process.argv[4];
+const textA = readFileSync(fpA, 'utf8');
+const textB = readFileSync(fpB, 'utf8');
+writeFileSync(fpC, textA + textB);
