32cb4212f868afa9bf5b577f0693829d98f62710
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-15 20:15:35 +0100

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,14 +3,14 @@
 class Rectangle {
   constructor (w, h) {
     if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
-	    this.width = w;
-	    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-	    console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 }
