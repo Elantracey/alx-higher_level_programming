2bc97fc57140ed4d0b37a6ccfc611bc1d396b33d
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-15 23:34:12 +0100

    Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 if (!isNaN(number)) {
-    console.log('My number: ' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
