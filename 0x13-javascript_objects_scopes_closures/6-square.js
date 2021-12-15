d11b3e53e4eeb400dc6816857e72e914b950d873
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-12-15 20:11:49 +0100

    Correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,10 @@
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-	    c = 'X';
+      c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-	    console.log(c.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 };
