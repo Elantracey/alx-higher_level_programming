7bcff08972020cbc01a7e252b1e5ab62e402da8a
Elantracey <84736535+Elantracey@users.noreply.github.com>
2022-01-31 02:51:06 -0800

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" attributes and methods of an object"""
-
-
-def lookup(obj):
-    """ attributes and methods of an object"""
-    return(dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" class MyList"""
-
-
-class MyList(list):
-    """ class MyList"""
-    def print_sorted(self):
-        """sort a list"""
-        print(sorted(self))
